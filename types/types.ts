c28ad80150c86230bdd967422f918855d3ad1242
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-09-01 18:10:52 +0900

    add types

--- types/types.ts
+++ types/types.ts
@@ -1,13 +1,16 @@
 export type Task = {
   id: string
-  created_ad: string
+  created_at: string
   title: string
   user_id: string | undefined
 }
 
 export type Notice = {
   id: string
-  created_ad: string
+  created_at: string
   content: string
   user_id: string | undefined
 }
+
+export type EditedTask = Omit<Task, 'created_at' | 'user_id'>
+export type EditedNotice = Omit<Notice, 'created_at' | 'user_id'>
