bc0db547a185ea22eb64c5f79accd8cf7cb2af3d
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-09-03 07:41:19 +0900

    add style

---
+++ components/Spinner.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Spinner: React.FC = () => {
+  return (
+    <div className='my-5 h-8 animate-spin rounded-full border-2 border-pink-600 border-t-transparent'></div>
+  )
+}
+
+export default Spinner
