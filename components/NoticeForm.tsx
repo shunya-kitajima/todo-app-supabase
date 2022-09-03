c7d2605ced7715bcca0c9c3d4e3fe2bf45f77798
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-09-03 21:58:57 +0900

    create NoticeForm component

---
+++ components/NoticeForm.tsx
@@ -0,0 +1,39 @@
+import React, { FormEvent } from 'react'
+import { supabase } from '../utils/supabase'
+import useStore from '../store'
+import { useMutateNotice } from '../hooks/useMutateNotices'
+
+export const NoticeForm: React.FC = () => {
+  const { editedNotice } = useStore()
+  const update = useStore((state) => state.updateEditedNotice)
+  const { createNoticeMutation, updateNoticeMutation } = useMutateNotice()
+  const submitHandler = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if (editedNotice.id === '') {
+      createNoticeMutation.mutate({
+        content: editedNotice.content,
+        user_id: supabase.auth.user()?.id,
+      })
+    } else {
+      updateNoticeMutation.mutate({
+        id: editedNotice.id,
+        content: editedNotice.content,
+      })
+    }
+  }
+
+  return (
+    <form onSubmit={submitHandler}>
+      <input
+        type="text"
+        className="my-2 rounded border border-gray-300 px-3 py-2 text-sm placeholder-gray-500 focus:border-indigo-500 focus:outline-none"
+        placeholder="New Notice ?"
+        value={editedNotice.content}
+        onChange={(e) => update({ ...editedNotice, content: e.target.value })}
+      />
+      <button className="ml-2 rounded bg-indigo-600 px-3 py-2 text-sm font-medium text-white hover:bg-indigo-700">
+        {editedNotice.id ? 'Update' : 'Create'}
+      </button>
+    </form>
+  )
+}
