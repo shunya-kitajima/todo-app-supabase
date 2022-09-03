b8879d8230eb5d1e3278a64d6aaa6f298084c3cd
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-09-03 14:43:01 +0900

    modify export

--- components/Spinner.tsx
+++ components/Spinner.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
 
-const Spinner: React.FC = () => {
+export const Spinner: React.FC = () => {
   return (
-    <div className='my-5 h-8 animate-spin rounded-full border-2 border-pink-600 border-t-transparent'></div>
+    <div className="my-5 h-8 animate-spin rounded-full border-2 border-pink-600 border-t-transparent"></div>
   )
 }
-
-export default Spinner
