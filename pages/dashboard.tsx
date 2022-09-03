2ef9d7fac6e114addd3687c6064eb28dffb478d5
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-09-03 21:32:00 +0900

    import tasks

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -20,8 +20,15 @@ const Dashboard: NextPage = () => {
         className="mb-6 h-6 w-6 cursor-pointer text-blue-500"
         onClick={signout}
       />
-      <TaskForm />
-      <TaskList />
+      <div className="grid grid-cols-2 gap-40">
+        <div>
+          <div className="my-3 flex justify-center">
+            <DocumentTextIcon className="y-8 h-8 text-blue-500" />
+          </div>
+          <TaskForm />
+          <TaskList />
+        </div>
+      </div>
     </Layout>
   )
 }
