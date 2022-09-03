df18c98a1cdce00bc963ce12da0952d6ac325c6a
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-09-03 22:12:44 +0900

    create NoticeItem component

---
+++ components/NoticeList.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useQueryNotices } from '../hooks/useQueryNotices'
+import { Spinner } from './Spinner'
+import { NoticeItem } from './NoticeItem'
+
+export const NoticeList: React.FC = () => {
+  const { data: notices, status } = useQueryNotices()
+
+  if (status === 'loading') return <Spinner />
+  if (status === 'error') return <p>{'Error'}</p>
+
+  return (
+    <div className="my-2">
+      {notices?.map((notice) => (
+        <NoticeItem key={notice.id} id={notice.id} content={notice.content} />
+      ))}
+    </div>
+  )
+}
